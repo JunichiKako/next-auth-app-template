ccbd0d3362d03fd15cf2500d226f709de863c4fb
JunichiKako <cjaucono@gmail.com>
2024-08-03 15:24:44 +0900

    認証ヘルパーの追加

--- auth.ts
+++ auth.ts
@@ -2,7 +2,7 @@ import NextAuth, { NextAuthConfig } from "next-auth";
 import Github from "next-auth/providers/github";
 
 export const config: NextAuthConfig = {
-  providers: [Github],
+  providers: [Github({clientId: process.env.AUTH_GITHUB_ID, clientSecret: process.env.AUTH_GITHUB_SECRET})],
   basePath: "/api/auth",
   callbacks: {
     authorized({ request, auth }) {
